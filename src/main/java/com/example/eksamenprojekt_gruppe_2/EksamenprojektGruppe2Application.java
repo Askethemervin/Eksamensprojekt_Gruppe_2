7ee2986b8d48f0cf33815b68e75bc50dd60e319d
Askethemervin <Aske.johanson@gmail.com>
2024-05-08 11:10:48 +0200

    Start af projekt

---
+++ src/main/java/com/example/eksamenprojekt_gruppe_2/EksamenprojektGruppe2Application.java
@@ -0,0 +1,13 @@
+package com.example.eksamenprojekt_gruppe_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenprojektGruppe2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamenprojektGruppe2Application.class, args);
+    }
+
+}

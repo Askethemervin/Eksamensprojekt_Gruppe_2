a56ddd0f8e048c2b2cea27d884c0e09e0af55142
uSerenOG <masv0002@stud.kea.dk>
2024-05-13 10:54:31 +0200

    Update

---
+++ src/main/java/com/example/eksamenprojekt_gruppe_2/Model/User.java
@@ -0,0 +1,18 @@
+package com.example.eksamenprojekt_gruppe_2.Model;
+
+public class User {
+
+private int id;
+private int telefonnummer;
+private String name;
+private String email;
+private String password;
+private String username;
+private String adress;
+
+public User() {
+}
+
+public User (String name, String email, String password, String username, String adress) {
+    this.name
+}

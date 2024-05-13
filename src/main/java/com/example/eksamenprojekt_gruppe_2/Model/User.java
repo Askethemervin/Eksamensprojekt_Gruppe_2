32ea19e2e1654fe5f26508f4ffd50b94b52b1bd8
uSerenOG <masv0002@stud.kea.dk>
2024-05-13 11:15:46 +0200

    nyt nyt

--- src/main/java/com/example/eksamenprojekt_gruppe_2/Model/User.java
+++ src/main/java/com/example/eksamenprojekt_gruppe_2/Model/User.java
@@ -3,16 +3,40 @@ package com.example.eksamenprojekt_gruppe_2.Model;
 public class User {
 
 private int id;
-private int telefonnummer;
-private String name;
-private String email;
-private String password;
 private String username;
-private String adress;
+private String password;
+private String email;
 
-public User() {
+public User () {
 }
 
-public User (String name, String email, String password, String username, String adress) {
-    this.name
-}
+    public User (String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User (int id, String username, String password, String email) {
+
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+

7080ba826797d621437f3ef5b022831f69b1cfd9
Askethemervin <Aske.johanson@gmail.com>
2024-05-13 11:34:54 +0200

    bil og lejeaftale klasser

---
+++ src/main/java/com/example/eksamenprojekt_gruppe_2/Model/Car.java
@@ -0,0 +1,9 @@
+package com.example.eksamenprojekt_gruppe_2.Model;
+
+public class Car {
+    int vehicleNumber;
+    String model;
+    int serialNumber;
+
+
+}
---
+++ src/main/java/com/example/eksamenprojekt_gruppe_2/Model/RentalAgreement.java
@@ -0,0 +1,49 @@
+package com.example.eksamenprojekt_gruppe_2.Model;
+
+public class RentalAgreement {
+    int id;
+    int type;
+    int duration;
+    int price;
+
+    public RentalAgreement(){
+
+    }
+    public RentalAgreement(int id, int type, int duration, int price) {
+        this.id = id;
+        this.type = type;
+        this.duration = duration;
+        this.price = price;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
